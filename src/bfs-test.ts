b507cc47072efe55bfaf7cd2301ed0504415e283
jmy48 <jmy48@cornell.edu>
2024-01-08 15:33:57 -0800

    comment out other things to focus on cross file testing

--- src/bfs-test.ts
+++ src/bfs-test.ts
@@ -1,30 +1,30 @@
-class A {
-    aField: number
-    constructor() {
-        this.aField = 3
-    }
-    M() {
-        const b = new B();
-        b.C();
-        b.bField2;
-        this.aField;
-    }
-}
+// class A {
+//     aField: number
+//     constructor() {
+//         this.aField = 3
+//     }
+//     M() {
+//         const b = new B();
+//         b.C();
+//         b.bField2;
+//         this.aField;
+//     }
+// }
 
-class B {
-    bField: number
-    bField2: number
-    bField3: number
-    constructor() {
-        this.bField = 4;
-        this.bField2 = 6;
-    }
+// class B {
+//     bField: number
+//     bField2: number
+//     bField3: number
+//     constructor() {
+//         this.bField = 4;
+//         this.bField2 = 6;
+//     }
 
-    C() {
-        this.bField;
-    }
+//     C() {
+//         this.bField;
+//     }
 
-    D() {
+//     D() {
 
-    }
-}
+//     }
+// }
--- src/sequence-generator.ts
+++ src/sequence-generator.ts
@@ -1,55 +1,55 @@
-abstract class SequenceGenerator<T> {
-    combiner: Combiner<T>;
-    windowSize: number;
-    generationLength: number;
+// abstract class SequenceGenerator<T> {
+//     combiner: Combiner<T>;
+//     windowSize: number;
+//     generationLength: number;
 
-    constructor(windowSize: number, generationLength: number, combiner: Combiner<T>) {
-        this.windowSize = windowSize;
-        this.generationLength = generationLength;
-        this.combiner = combiner;
-    }
+//     constructor(windowSize: number, generationLength: number, combiner: Combiner<T>) {
+//         this.windowSize = windowSize;
+//         this.generationLength = generationLength;
+//         this.combiner = combiner;
+//     }
 
-    generateSequence(): T[] {
-        var sequence = this.getStartingSequence();
-        for (var i = 0; i < this.generationLength; i++) {
-            const latest_window = sequence.slice(sequence.length - this.windowSize);
-            sequence.push(this.combiner.combine(latest_window))
-        }
-        return sequence;
-    }
+//     generateSequence(): T[] {
+//         var sequence = this.getStartingSequence();
+//         for (var i = 0; i < this.generationLength; i++) {
+//             const latest_window = sequence.slice(sequence.length - this.windowSize);
+//             sequence.push(this.combiner.combine(latest_window))
+//         }
+//         return sequence;
+//     }
 
-    abstract getStartingSequence(): T[];
-}
+//     abstract getStartingSequence(): T[];
+// }
 
 
-interface Combiner<T> {
-    combine(l: T[]): T
-}
+// interface Combiner<T> {
+//     combine(l: T[]): T
+// }
 
-class NumberAdder implements Combiner<number> {
-    combine(l: number[]): number {
-        return l.reduce((prev, curr) => prev + curr)
-    }
-}
+// class NumberAdder implements Combiner<number> {
+//     combine(l: number[]): number {
+//         return l.reduce((prev, curr) => prev + curr)
+//     }
+// }
 
-class StringConcatenator implements Combiner<string> {
-    combine(l: string[]): string {
-        return l.reduce((prev, curr) => prev + curr)
-    }
-}
+// class StringConcatenator implements Combiner<string> {
+//     combine(l: string[]): string {
+//         return l.reduce((prev, curr) => prev + curr)
+//     }
+// }
 
-class ConcreteSequenceGenerator extends SequenceGenerator<string> {
-    constructor(generationLength: number) {
-        super(2, generationLength, new StringConcatenator());
-    }
+// class ConcreteSequenceGenerator extends SequenceGenerator<string> {
+//     constructor(generationLength: number) {
+//         super(2, generationLength, new StringConcatenator());
+//     }
 
-    getStartingSequence(): string[] {
-        return ["hi", "bye"];
-    }
-}
+//     getStartingSequence(): string[] {
+//         return ["hi", "bye"];
+//     }
+// }
 
-function main() {
-    console.log((new ConcreteSequenceGenerator(10)).generateSequence());
-}
+// function main() {
+//     console.log((new ConcreteSequenceGenerator(10)).generateSequence());
+// }
 
-main();
+// main();
